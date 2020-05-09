651ad42f4be12aa5ae0a4eee90d5be1e8d99b5fc
Andricsak Alexandru <38785216+Alexinio97@users.noreply.github.com>
2020-05-10 01:10:10 +0300

    Removed credentials

--- react-veterinary-app/src/firebaseConfig/config.js
+++ react-veterinary-app/src/firebaseConfig/config.js
@@ -5,14 +5,7 @@ import "firebase/firebase-storage";
 import "firebase/messaging";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCkAWg2p5G7FIV4WFFRNWdhopm15LALxUQ",
-    authDomain: "final-year-project-748be.firebaseapp.com",
-    databaseURL: "https://final-year-project-748be.firebaseio.com",
-    projectId: "final-year-project-748be",
-    storageBucket: "final-year-project-748be.appspot.com",
-    messagingSenderId: "1007783445768",
-    appId: "1:1007783445768:web:e1b873c5de25a8e0cd18bd",
-    measurementId: "G-1TLB441458"
+    <config_here>
 };
 // Initialize Firebase
 export const myFirebase = firebase.initializeApp(firebaseConfig);
